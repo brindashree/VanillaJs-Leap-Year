4fe2ef1051aa50aa5c433f432c099a4768b40589
Brindashree C B <brindashree@users.noreply.github.com>
2020-12-26 12:40:41 +0530

    added test code for leap year check

---
+++ app.js
@@ -0,0 +1,24 @@
+var output = document.querySelector('#output');
+var input = document.querySelector('#userinput');
+
+var checkbtn = document.querySelector('#btn');
+
+checkbtn.addEventListener("click", isLeapYear);
+
+
+
+function isLeapYear() {
+    var year = input.value;
+    console.log(year);
+    if (year % 400 == 0) {
+        output.innerText = 'You are born in a LEAP YEAR';
+    }
+    else if (year % 100 == 0) {
+         output.innerText = 'You are NOT born in a LEAP YEAR';
+    }
+    else if (year % 4 == 0) {
+        output.innerText = 'You are born in a LEAP YEAR';
+    } else {
+        output.innerText = 'You are NOT born in a LEAP YEAR';
+    }
+}
