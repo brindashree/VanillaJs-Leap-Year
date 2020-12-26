69ba29b7545cb788aaebbcc1cb1f63d92c1e5286
Brindashree C B <brindashree@users.noreply.github.com>
2020-12-26 13:39:23 +0530

    done

--- app.js
+++ app.js
@@ -9,16 +9,20 @@ checkbtn.addEventListener("click", isLeapYear);
 
 function isLeapYear() {
     var year = input.value;
-    console.log(year);
-    if (year % 400 == 0) {
-        output.innerText = 'You are born in a LEAP YEAR';
-    }
-    else if (year % 100 == 0) {
-         output.innerText = 'You are NOT born in a LEAP YEAR';
-    }
-    else if (year % 4 == 0) {
-        output.innerText = 'You are born in a LEAP YEAR';
-    } else {
-        output.innerText = 'You are NOT born in a LEAP YEAR';
-    }
+    // console.log(year);
+    
+     
+
+        if (year % 400 == 0) {
+            output.innerText = 'You are born in a LEAP YEAR';
+        }
+        else if (year % 100 == 0) {
+            output.innerText = 'You are NOT born in a LEAP YEAR';
+        }
+        else if (year % 4 == 0) {
+            output.innerText = 'You are born in a LEAP YEAR';
+        } else {
+            output.innerText = 'You are NOT born in a LEAP YEAR';
+        }
+    
 }
